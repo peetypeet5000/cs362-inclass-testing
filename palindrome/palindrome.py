87f44027f402bedc74bdb0d24e92b98bcebf7782
Peter LaMontagne <49214098+peetypeet5000@users.noreply.github.com>
2021-05-14 10:16:00 -0700

    palindrome program

---
+++ palindrome/palindrome.py
@@ -0,0 +1,15 @@
+from operator import truediv
+from pickle import FALSE
+
+
+def isPalindrome(inputString):
+    i = 0
+
+    for ele in range(len(inputString)):
+        if inputString[ele] == inputString[len(inputString)- 1 - i]:
+            i = i + 1
+
+    if i = len(inputString):
+        return True
+    else:
+        return False
